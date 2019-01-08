c7e71f294c21da83fbcc2077f9025652a2f62ca9
Adolf-san <aespinosaperes21@gmail.com>
2019-01-08 18:18:27 -0300

    practicando un poco

---
+++ contador_de_vocales_consonantes_espacios_etc.py
@@ -0,0 +1,30 @@
+frase_usuario = input("introduce una frase: ")
+
+vocales = ["a", "e", "i", "o", "u"]
+espacios = [" "]
+comas = [","]
+puntos = ["."]
+
+n_vocales = 0
+n_consonantes = 0
+n_espacios = 0
+n_comas = 0
+n_puntos = 0
+
+for letra in frase_usuario:
+    if letra in vocales:
+        n_vocales += 1
+    if letra in espacios:
+        n_espacios += 1
+    if letra in comas:
+        n_comas += 1
+    if letra in puntos:
+        n_puntos += 1
+    else:
+        n_consonantes += 1
+
+print("las vocales son {}".format(n_vocales))
+print("las consonantes son {}".format(n_consonantes))
+print("las comas son {}".format(n_comas))
+print("las puntos son {}".format(n_puntos))
+print("los espacios son {}".format(n_espacios))
